7acde3d6f187a7f47d2a15df4ab510abc91f0efc
Florent Thiéry <florent.thiery@ubicast.eu>
2015-06-05 09:47:15 +0200

    add gobject test, make test.py an actual test

--- test/test_gobject.py
+++ test/test_gobject.py
@@ -3,7 +3,9 @@
 # Copyright 2015, Florent Thiery 
 
 from __future__ import print_function
+import gobject
 import easyevent
+easyevent.event.dispatcher = "gobject"
 
 class Listener(easyevent.User):
   def __init__(self):
@@ -11,10 +13,9 @@ class Listener(easyevent.User):
     self.register_event("speech")
     
   def evt_speech(self, event):
-    print("Got speech event, : %s" %event.content)
+    print("Got speech event: %s" %event.content)
     print("Test success, quitting")
     self.unregister_event("speech")
-    #self.unregister_all_events()
     import sys
     sys.exit(0)
     
@@ -28,7 +29,13 @@ class Shouter(easyevent.User):
 if __name__ == "__main__":
     l = Listener()
     s = Shouter()
-    s.shout("hello world")
-    # at that point, Listener prints "Got speech event: hello world"
-    print("Event not received, test failed")
-    sys.exit(1)
+    gobject.idle_add(s.shout, "hello world")
+
+    def failure():
+        print('Test failed')
+        import sys
+        sys.exit(1)
+    gobject.timeout_add_seconds(1, failure)
+
+    loop = gobject.MainLoop()
+    loop.run()
